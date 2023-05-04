e77dc554cb8e3ec01d999c420caddc0ee124420c
Nvm35 <twikk3@gmail.com>
2023-05-04 19:01:12 +0600

    33

--- src/main/java/com/epam/conditions/BitwiseValuesSwap.java
+++ src/main/java/com/epam/conditions/BitwiseValuesSwap.java
@@ -1,8 +1,18 @@
 package com.epam.conditions;
 
 public class BitwiseValuesSwap {
-
     public void swap(int first, int second) {
-    }
+        System.out.println("Before swap:");
+        System.out.println("first = " + first);
+        System.out.println("second = " + second);
 
+        first ^= second;
+        second ^= first;
+        first ^= second;
+
+        System.out.println("After swap:");
+        System.out.println("first = " + first);
+        System.out.println("second = " + second);
+    }
 }
+
--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -1,9 +1,37 @@
 package com.epam.conditions;
 
 public class DaysInMonth {
-
     public void printDays(int year, int month) {
+        if (year < 0) {
+            System.out.println("Invalid date");
+            return;
+        }
+        if (month < 1 || month > 12) {
+            System.out.println("Invalid date");
+            return;
+        }
 
+        int daysInMonth;
+        switch (month) {
+            case 2:
+                if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+                    daysInMonth = 29;
+                } else {
+                    daysInMonth = 28;
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                daysInMonth = 30;
+                break;
+            default:
+                daysInMonth = 31;
+                break;
+        }
+
+        System.out.println("The number of days in month " + month + " of year " + year + " is " + daysInMonth);
     }
-
 }
+
