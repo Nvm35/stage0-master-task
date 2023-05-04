0a4d345bec6f46d0c55589d317d176a31581f594
Nvm35 <twikk3@gmail.com>
2023-05-04 18:43:56 +0600

    22

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,13 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
-    }
+        int thousandsDigit = number / 1000;
+        int hundredsDigit = (number / 100) % 10;
+        int tensDigit = (number / 10) % 10;
+        int onesDigit = number % 10;
 
+        int sum = thousandsDigit + hundredsDigit + tensDigit + onesDigit;
+
+        System.out.println(sum);
+    }
 }
--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,13 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int onesDigit = number % 10;
+        int tensDigit = (number / 10) % 10;
+        int hundredsDigit = (number / 100) % 10;
+
+        int reversedNumber = onesDigit * 100 + tensDigit * 10 + hundredsDigit;
+
+        System.out.println(reversedNumber);
     }
 
 }
